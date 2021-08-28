167201c2a068c34b1a4fa5cc429024b76c05ed24
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-28 19:04:45 +0530

    user model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    choices = (
+        ('0','Teacher'),
+        ('1','Student'),
+        ('2','Company/Organisation/School/Institute'),
+    )
+
+    username=None
+    first_name = None
+    last_name = None
+    name = models.CharField(max_length=100,blank=True)
+    phone_number = models.CharField(max_length=20,unique=True,blank=True)
+    email = models.EmailField(max_length=255,unique=True)
+    user_type = models.CharField(max_length=10,choices=choices,default='1')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+
