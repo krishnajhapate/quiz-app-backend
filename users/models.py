59319264e0a9faad1d8f844c1d35254708506777
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-28 19:19:36 +0530

    custom manager created

--- users/models.py
+++ users/models.py
@@ -1,7 +1,35 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser ,BaseUserManager
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError("Email must be set")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def creat_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefaults('is_staff',True)
+        extra_fields.setdefaults('is_superuser',True)
+        extra_fields.setdefaults('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must be is_staff=True") 
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must be is_superuser=True")
+
+        return self.create_user(email,password,**extra_fields)
+
+    pass
+
 
 class User(AbstractUser):
     choices = (
@@ -17,6 +45,8 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20,unique=True,blank=True)
     email = models.EmailField(max_length=255,unique=True)
     user_type = models.CharField(max_length=10,choices=choices,default='1')
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
