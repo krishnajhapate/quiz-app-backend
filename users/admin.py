f51d462f9c916bc0d5d0fa4aa9bde362562dad3d
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-30 14:15:08 +0530

    added custom user adding form to admin panel

--- users/admin.py
+++ users/admin.py
@@ -5,10 +5,21 @@ from .forms import UserCreationForm
 
 class UserAdmin(admin.ModelAdmin):
     add_form = UserCreationForm
-    list_display = ("email","name","user_type")
+    list_display = ("email","name","user_type",)
     ordering = ("email",)
 
-    
+    fieldsets = (
+        ('Personal Details',{
+            "fields":('name','email',"phone","user_type")
+        }),
+    )
+    add_fieldsets =(
+        (None,
+        {
+            'classes':('wide',),
+            'fields':('name','email','password','phone','user_type'),
+            }),
+    )
     
 
 admin.site.register(User,UserAdmin)
